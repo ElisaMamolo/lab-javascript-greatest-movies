d3502d7884f48cde5047b3f82113735750249c1a
ElisaMamolo <49635343+ElisaMamolo@users.noreply.github.com>
2022-01-07 19:02:11 +0100

    dramamoviesscore

--- src/movies.js
+++ src/movies.js
@@ -2064,25 +2064,14 @@ scoresAverage(movies);
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
 function dramaMoviesScore(movies) {
-  let counter = 0;
-  if (movies.length === 0 ){
-    return 0;
-  }
+  //haven't thought of this, reusing the other function
+  let dramaMoviesArray = movies.filter(function(movie) {
+    return movie.genre.includes('Drama');
+  });
 
-  const scoreAverage = movies.reduce(function (sum, movie) {
-    if(movie.score && movie.genre.includes("Drama")) {
-      return sum + movie.score;
-    } else {
-      //should return average even if one of the movies does not have score
-      return sum;
-    }
-  }, 0);
-
-  return Number(scoreAverage/movies.length);
-  //console.log(counter);
+  return scoresAverage(dramaMoviesArray);
 }
 
-dramaMoviesScore(movies);
 
 // Iteration 5: Ordering by year - 
 //Order by year, ascending (in growing order)
